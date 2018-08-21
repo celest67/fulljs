bda065aafae9c5d4aa0e2c54687584a3c8aff944
pms <pmarin.sariego@gmail.com>
2018-08-21 11:43:05 -0300

    1.4 http y https

--- server.js
+++ server.js
@@ -1,6 +1,33 @@
-//import './config'
-import config, { nodeEnv, logStars } from './config';
+import http from 'http';
 
-console.log(config, nodeEnv)
+const server = http.createServer((req, res) => {
+    res.write('Hello HTTP!\n');
+    setTimeout(() => {
+        res.write('Dp de 3');
+        res.end();
+    }, 3000);
+});
 
-logStars('Inicio');
+server.listen(8080);
+
+/*
+const server = http.createServer();
+
+server.listen(8080);
+
+server.on('request', (req, res) => {
+    res.write('Hello HTTP!\n');
+    setTimeout(() => {
+        res.write('Dp de 3');
+        res.end();
+    }, 3000);
+});*/
+/*import https from 'https'
+
+https.get('https://www.lynda.com',res => {
+    console.log('Response status code: ', res.statusCode)
+
+    res.on('data', chunk => {
+        console.log(chunk.toString());
+    });
+});*/
