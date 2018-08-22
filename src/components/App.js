01e7d211c172893886ea85356bd02c932bc16f61
pms <pmarin.sariego@gmail.com>
2018-08-22 11:03:35 -0300

    1.11 Components State

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import Header from './Header'
 
-const App = () => {
-    return (
-        <div className="App">
-            <Header message="Naming lala"/>
-            <div>
-                ...
+class App extends React.Component {
+    /*constructor(props) {
+        super(props);
+        this.state = {
+            test: 42
+        };
+    }*/
+    state = {
+        test: 43,
+        pageHeader: 'Naming Contests'
+    };
+    render () {
+        return (
+            <div className="App">
+                <Header message={this.state.pageHeader}/>
+                <div>
+                    {this.state.test}
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default App;
